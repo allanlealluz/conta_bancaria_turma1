30c463b9e83c3ae07a7e7a3c38af5f1570624034
ALLONE <allanlealluz@gmail.com>
2025-09-25 10:32:04 -0300

    ..

--- src/main/java/com/senai/conta_bancaria_turma1/interface_ui/ClienteController.java
+++ src/main/java/com/senai/conta_bancaria_turma1/interface_ui/ClienteController.java
@@ -1,12 +1,15 @@
 package com.senai.conta_bancaria_turma1.interface_ui;
 
+import com.senai.conta_bancaria_turma1.application.dto.ClienteAtualizadoDTO;
 import com.senai.conta_bancaria_turma1.application.dto.ClienteRegistroDTO;
 import com.senai.conta_bancaria_turma1.application.dto.ClienteResponseDTO;
 import com.senai.conta_bancaria_turma1.application.service.ClienteService;
-import com.senai.conta_bancaria_turma1.domain.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -14,26 +17,31 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteController {
 
     private final ClienteService service;
+
     @PostMapping
     public ResponseEntity<ClienteResponseDTO> registrarCliente(@RequestBody ClienteRegistroDTO dto) {
-        return service.registrarCliente(dto);
+        ClienteResponseDTO novoCliente = service.registrarCliente(dto);
+
+        return ResponseEntity.created(URI.create("/api/cliente/cpf/"+novoCliente.cpf())
+        ).body(novoCliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDTO>> listarClientesAtivos(){
+        return ResponseEntity.ok(service.listarClientesAtivos());
     }
     @GetMapping("/{id}")
     public ResponseEntity<ClienteResponseDTO> obterCliente(@PathVariable String id) {
         return ResponseEntity.ok().build();
     }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> removerCliente(@PathVariable String id) {
-        return ResponseEntity.ok().build();
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<ClienteResponseDTO> buscarClienteAtivoPorCpf(@PathVariable String cpf){
+        return ResponseEntity.ok(service.buscarClienteAtivoPorCpf(cpf));
     }
+
     @PutMapping("/{cpf}")
-    public ResponseEntity<Void> atualizarCliente(@PathVariable String cpf) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ClienteResponseDTO> atualizarCliente(@PathVariable String cpf,
+                                                               @RequestBody ClienteAtualizadoDTO dto) {
+        return ResponseEntity.ok(service.atualizarCliente(cpf, dto));
     }
-
-    @GetMapping
-    public ResponseEntity<Void> listarClientes() {
-        return ResponseEntity.ok().build();
-    }
-
 }
